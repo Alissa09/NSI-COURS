d6bacc1cb3b599f94741563b9bf28c86dd9e7ba3
alissa09 <alissa.cerqueira@icloud.com>
2022-02-07 11:44:12 +0100

    maj

---
+++ projet2.py
@@ -0,0 +1,20 @@
+import pygame, sys
+from pygame.locals import *
+from random import randint
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((1200, 1000))
+
+perso = pygame.image.load("homer2.png").convert_alpha()
+
+position_perso = perso.get_rect()
+
+while True :
+    fenetre.fill([10,186,181])
+    position_perso.topleft = (randint(0,540),randint(0,380))
+    fenetre.blit(perso, position_perso)
+    pygame.display.flip()
+    pygame.time.delay(1000)
+
+
